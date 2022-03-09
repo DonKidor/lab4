bdc16111df9fb8d32dc626b7564e018439ce113e
Mihail <JIaIILLIa3@yandex.ru>
2022-03-09 16:20:12 +0700

    Create git project

---
+++ lab33/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab33.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
